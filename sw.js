adb641edd0afef373e1274a2f0b0d6ce13376c07
woojehong <hwj.rouget@gmail.com>
2026-08-05 19:02:34 +0900

    Deploy service worker and accessibility improvements

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
-self.addEventListener('fetch', function(event) { });
+const HOOJE_CACHE_PREFIX = 'hooje-studio-';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const names = await caches.keys();
+    await Promise.all(
+      names
+        .filter(name => name.startsWith(HOOJE_CACHE_PREFIX))
+        .map(name => caches.delete(name))
+    );
+    await self.clients.claim();
+  })());
+});
